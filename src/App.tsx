efd4df9714501471ba73c5d3605c013689faf72e
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-04-17 16:42:00 -0500

    Finishing components

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Componente from './components/Componente';
 import {Properties} from './components/Properties';
 import './App.css';
 
-const elArreglo = ['a',1,2,3,'b'];
+const elArreglo = ['a',1,2,3,'b','4'];
 
 function App() {
   return (
@@ -11,7 +11,15 @@ function App() {
       <section>
         <Componente />
         <hr/>
-        <Properties firstName="Fabian" arreglo={elArreglo} booleano={true} objeto={{nombre:'Fabian', email:'joshefbeltran@gmail.com'}}></Properties>
+        <Properties 
+          firstName="Fabian" 
+          arreglo={elArreglo} 
+          booleano={true} 
+          objeto={{nombre:'Fabian', email:'joshefbeltran@gmail.com'}}
+          elementoReact = {<i>Esto es un elemento react</i>}
+          funcion={(num:number) => num*num}
+          componenteReact = {<Componente/>}
+        />
       </section>
     </div>
   );
--- src/components/Properties.tsx
+++ src/components/Properties.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 type Props = {
     firstName: string,
     arreglo: (number|string)[],
-    booleano: boolean;
+    booleano: boolean,
     objeto : {
         nombre: string,
         email: string
-    }
+    },
+    elementoReact: JSX.Element,
+    funcion: any,
+    componenteReact: any
 };
 
 export const Properties = (props: Props): JSX.Element => {
@@ -17,6 +20,9 @@ export const Properties = (props: Props): JSX.Element => {
                 <li>arreglo:{props.arreglo.join(", ")}</li>
                 <li>Booleano: {props.booleano ? 'Verdadero' : 'Falso' }</li>
                 <li>Objeto: {props.objeto.nombre + ' ' + props.objeto.email}</li>
+                <li>{props.elementoReact}</li>
+                <li>{props.arreglo.map(props.funcion).join(", ")}</li>
+                <li>{props.componenteReact}</li>
             </ul>
         </>
     )
