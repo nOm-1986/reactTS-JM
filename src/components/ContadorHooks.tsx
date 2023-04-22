bbd665bf45d49032240726caf99db8cfb00dc215
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-04-22 12:41:52 -0500

    using destructuration on props arguments to avoid props.title ...

--- src/components/ContadorHooks.tsx
+++ src/components/ContadorHooks.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 
-export const ContadorHooks = (props: {titulo: string}): JSX.Element => {
+//Esta seria la forma tradicional.
+//export const ContadorHooks = (props: {titulo: string}): JSX.Element => {
+//De esta forma aplico directamente la destructuración y me ahorro despues el props.titulo
+export const ContadorHooks = ({titulo}: {titulo: string}): JSX.Element => {
 
     const [contador, setContador] = useState(0);
 
@@ -9,7 +12,7 @@ export const ContadorHooks = (props: {titulo: string}): JSX.Element => {
 
     return(
         <>
-            <h2>Hook - useState - {props.titulo}</h2>
+            <h2>Hook - useState - {titulo}</h2>
             <nav>
                 <button onClick = {sumar}>➕</button>
                 <button onClick = {restar}>➖</button>
