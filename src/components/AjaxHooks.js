f6a2dde394f568e04748ec6b71a6c1624ed16339
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-05-02 09:27:30 -0500

    AjaxHook

--- src/components/AjaxHooks.js
+++ src/components/AjaxHooks.js
@@ -13,13 +13,31 @@ export default function AjaxHooks() {
     const [pokemons, setPokemons] = useState([]);
 
     useEffect(() => {
-        
+        //Esta es la forma correcta de usar una función asincrona dentro del useEffect.
+        //Si pongo el Async dentro de callback del useEffect se considera un antipatron. -> Mala practica.
+        const getPokemons = async (url) => {
+            let res = await fetch(url);
+            let json = await res.json();
+
+            json.results.forEach(async (el) => {
+                let res = await fetch(el.url),
+                    json = await res.json();
+
+                let pokemon = {
+                    id: json.id,
+                    name: json.name,
+                    avatar: json.sprites.front_default
+                };
+                setPokemons((pokemons) => [...pokemons, pokemon]);
+            });
+        }
+        getPokemons("https://pokeapi.co/api/v2/pokemon/");
     }, []);
 
     return (
         <>
             <h1>Peticiones Asíncronas en Hooks</h1>
-            {pokemons.length == 0 ? (<h2>Cargando...</h2>):(
+             {pokemons.length == 0 ? (<h2>Cargando...</h2>):(
                 pokemons.map((el) => (
                     <Pokemon key={el.id} name={el.name} avatar={el.avatar} />
                 ))
